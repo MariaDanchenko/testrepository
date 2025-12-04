35d274f44100c3e49891749da1e6a7f142b78eca
Maria <mashadanchenko75@gmail.com>
2025-12-04 14:41:17 +0300

    feat(): init project

--- src/test/java/patterns/CartPage.java
+++ src/test/java/patterns/CartPage.java
@@ -2,6 +2,9 @@ package patterns;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class CartPage {
 
@@ -11,6 +14,8 @@ public class CartPage {
     private final By shoppingCartLink = By.cssSelector("[data-test = 'shopping-cart-link']");
     private final By removeButton = By.cssSelector("button[data-test^ = 'remove-']");
     private final By checkoutButton = By.cssSelector("button[data-test = 'checkout']");
+    private final By cartList = By.cssSelector(".cart_item");
+    private final By itemName = By.cssSelector(".inventory_item_name");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
@@ -25,8 +30,30 @@ public class CartPage {
         return Integer.parseInt(countText);
     }
 
-    public void removeProduct() {
-        driver.findElement(removeButton).click();
+    public void removeProductFromCart(String nameOfProduct) {
+        List<WebElement> items = driver.findElements(cartList);//получаем список
+
+        for (int i = 0; i < items.size(); i++) {
+            WebElement item = items.get(i);
+            String name = item.findElement(itemName).getText();//получаем название
+            if (name.equals(nameOfProduct)) {
+                item.findElement(removeButton).click();
+                break;
+            }
+        }
+    }
+
+    public boolean isProductInCart(String nameOfProduct) {
+        List<WebElement> items = driver.findElements(cartList);
+
+        for (int i = 0; i < items.size(); i++) {
+            WebElement item = items.get(i);
+            String name = item.findElement(itemName).getText();
+            if (name.equals(nameOfProduct)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void checkout() {
