a487e3b07a1818bfc73ddcc4a329db303004310e
Maria <mashadanchenko75@gmail.com>
2025-12-11 17:56:41 +0300

    feat(): init project

--- src/test/java/waits/AddToCartButtonTest.java
+++ src/test/java/waits/AddToCartButtonTest.java
@@ -1,7 +1,12 @@
 package waits;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class AddToCartButtonTest extends BaseTest {
 
@@ -9,9 +14,15 @@ public class AddToCartButtonTest extends BaseTest {
     public void testAddToCartButton() {
         login();
 
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.clickAddToCartButton();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        Assert.assertEquals(inventoryPage.getRemoveButtonText(), "Remove");
+        By addToCartButton = By.cssSelector("button[data-test='add-to-cart-sauce-labs-backpack']");
+        By removeButton   = By.cssSelector("button[data-test='remove-sauce-labs-backpack']");
+
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButton)).click();
+
+        String removeText = driver.findElement(removeButton).getText();
+
+        Assert.assertEquals(removeText, "Remove");
     }
 }
