d907f104db1a383ed54fd0f33f239ed934799798
Maria <mashadanchenko75@gmail.com>
2025-12-11 15:59:25 +0300

    feat(): init project

---
+++ src/test/java/waits/LoginPage.java
@@ -0,0 +1,35 @@
+package waits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    protected WebDriver driver;
+
+    private final By usernameField = By.cssSelector("#user-name");
+    private final By passwordField = By.cssSelector("#password");
+    private final By loginButton = By.cssSelector("#login-button");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+}
