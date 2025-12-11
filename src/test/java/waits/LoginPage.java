5f22d5d7ad1374d7e238d47a3d9d194145d4d1e0
Maria <mashadanchenko75@gmail.com>
2025-12-11 19:06:25 +0300

    feat(): init project

--- src/test/java/waits/LoginPage.java
+++ src/test/java/waits/LoginPage.java
@@ -12,18 +12,22 @@ public class LoginPage {
     private final By loginButton = By.cssSelector("#login-button");
 
     public LoginPage(WebDriver driver) {
+
         this.driver = driver;
     }
 
     public void enterUsername(String username) {
+
         driver.findElement(usernameField).sendKeys(username);
     }
 
     public void enterPassword(String password) {
+
         driver.findElement(passwordField).sendKeys(password);
     }
 
     public void clickLogin() {
+
         driver.findElement(loginButton).click();
     }
 
