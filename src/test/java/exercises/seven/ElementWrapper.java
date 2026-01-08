856146cfad07295fe268704c871764ab497bff19
Maria <mashadanchenko75@gmail.com>
2026-01-08 18:15:54 +0300

    feat(): init project

---
+++ src/test/java/exercises/seven/ElementWrapper.java
@@ -0,0 +1,6 @@
+package exercises.seven;
+
+public class ElementWrapper {
+
+
+}
--- src/test/java/generics/AttributeHandler.java
+++ src/test/java/generics/AttributeHandler.java
@@ -3,7 +3,6 @@ package generics;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AttributeHandler {
