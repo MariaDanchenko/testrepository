4494cc7efcd42aafde45883538088cc6a21f6e44
Maria <mashadanchenko75@gmail.com>
2026-01-08 18:07:40 +0300

    feat(): init project

---
+++ src/test/java/exercises/Checkbox.java
@@ -0,0 +1,11 @@
+package exercises;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class Checkbox {
+
+    private String id;
+    private boolean checked;
+}
---
+++ src/test/java/exercises/CheckboxesPage.java
@@ -0,0 +1,27 @@
+package exercises;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class CheckboxesPage {
+
+    @FindBy(css = "input[type='checkbox']")
+    private List<WebElement> checkboxes;
+
+    public CheckboxesPage(WebDriver driver) {
+
+        PageFactory.initElements(driver, this);
+    }
+
+    public List<WebElement> getCheckboxes() {
+        return checkboxes;
+    }
+
+    public void toggleCheckbox(int index) {
+        checkboxes.get(index).click();
+    }
+}
