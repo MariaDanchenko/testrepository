cfad4e030f2da0eb956bc38d4a03da339cab1263
Maria <mashadanchenko75@gmail.com>
2026-01-08 20:56:13 +0300

    feat(): init project

---
+++ src/test/java/exercises/seven/CheckboxesTest.java
@@ -0,0 +1,42 @@
+package exercises.seven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckboxesTest {
+
+    private WebDriver driver;
+    private CheckboxesPage checkboxesPage;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        checkboxesPage = new CheckboxesPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testCheckboxes() {
+
+        List<ElementWrapper<WebElement>> wrappers = new ArrayList<>();
+
+    }
+}
