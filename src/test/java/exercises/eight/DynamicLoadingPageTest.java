e24dcdb15832bd42c87626f6af7855f28257e1c4
Maria <mashadanchenko75@gmail.com>
2026-01-09 16:00:25 +0300

    feat(): init project

---
+++ src/test/java/exercises/eight/DynamicLoadingPageTest.java
@@ -0,0 +1,40 @@
+package exercises.eight;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DynamicLoadingPageTest {
+
+    private WebDriver driver;
+    private DynamicLoadingPage dynamicLoadingPage;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading");
+
+        dynamicLoadingPage = new DynamicLoadingPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testDynamicLoadingPage() {
+        dynamicLoadingPage.clickLink();
+        dynamicLoadingPage.clickStartButton();
+
+        Assert.assertEquals(dynamicLoadingPage.getLoadingMessage(), "Hello World!");
+    }
+}
