c17c63bda8a3960ab9f119b9bfb0fa3d00c6d181
Maria <mashadanchenko75@gmail.com>
2026-01-09 16:24:41 +0300

    feat(): init project

--- src/test/java/exercises/nine/DynamicTest.java
+++ src/test/java/exercises/nine/DynamicTest.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-public class DynamicLoadingPageTest {
+@Listeners(CustomTestListener.class)
+public class DynamicTest {
 
     private WebDriver driver;
     private exercises.eight.DynamicLoadingPage dynamicLoadingPage;
