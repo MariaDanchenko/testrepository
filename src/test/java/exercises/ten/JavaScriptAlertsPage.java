185da1861f7bb52997b7ac7cb934229e52796226
Maria <mashadanchenko75@gmail.com>
2026-01-10 17:59:45 +0300

    feat(): init project

--- src/test/java/exercises/ten/JavaScriptAlertsPage.java
+++ src/test/java/exercises/ten/JavaScriptAlertsPage.java
@@ -9,11 +9,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class JavaScriptAlertsPage {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private final Logger logger = LogManager.getLogger(JavaScriptAlertsPage.class);
 
     @FindBy(css = "button[onclick = 'jsAlert()']")
     private WebElement jsAlert;
@@ -31,50 +34,61 @@ public class JavaScriptAlertsPage {
         PageFactory.initElements(driver, this);
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        logger.info("JavaScriptAlertsPage initialized");
     }
 
     public void clickJSAlert() {
         jsAlert.click();
+        logger.info("Clicked on JS Alert button");
 
     }
 
     public void clickJSConfirm() {
         jsConfirm.click();
+        logger.info("Clicked on JS Confirm button");
     }
 
     public void clickJSPrompt() {
         jsPrompt.click();
+        logger.info("Clicked on JS Prompt button");
     }
 
     public Alert waitForAlert() {
         wait.until(ExpectedConditions.alertIsPresent());
+        logger.info("Alert is present");
         return driver.switchTo().alert();
     }
 
     public String getAlertText() {
         Alert alert = waitForAlert();
         String alertText = alert.getText();
+        logger.info("Alert text: {}", alertText);
         return alertText;
     }
 
     public void acceptAlert() {
         Alert alert = waitForAlert();
         alert.accept();
+        logger.info("Alert accepted");
     }
 
     public void dismissAlert() {
         Alert alert = waitForAlert();
         alert.dismiss();
+        logger.info("Alert dismissed");
     }
 
     public void sendKeysToPrompt(String text) {
         Alert alert = waitForAlert();
         alert.sendKeys(text);
+        logger.info("Text '{}' sent to prompt", text);
         alert.accept();
+        logger.info("Prompt accepted");
     }
 
     public String getResultText() {
         String result = resultText.getText();
+        logger.info("Result text: {}", result);
         return result;
     }
 }
